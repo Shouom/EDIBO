f14db4af3ec6e5763df306c996ece8d100bdf6d2
Shouom <ernestsmezals@gmail.com>
2021-04-09 16:06:33 +0300

    Flask file added

---
+++ Python/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+	return "<h1>Hello you!</h1>"
+
+if __name__ == '__main__':
+    app.run(port=5000)
+
+ufile = open('user.txt','r')
+pfile = open('password.txt','r')
+
+app.config['MYSQL_USER'] = ufile.read()
+ufile.close()
+app.config['MYSQL_PASSWORD'] = pfile.read()
+pfile.close()
+app.config['MYSQL_DB'] = ''
