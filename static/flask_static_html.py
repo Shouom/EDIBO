5f0e9d016c436c7b1112e841c20480f14ab9ad63
Shouom <80401573+Shouom@users.noreply.github.com>
2021-05-13 17:12:39 +0300

    Fixed flask script

---
+++ static/flask_static_html.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+import MySQLdb
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return "<h1>Hello you!</h1>"
+ufile = open('user.txt','r')
+user = ufile.read()[:-1] # pēdējais simbols ir rindas beigas pazīme, mums to nevajag
+ufile.close()
+pfile = open('password.txt','r')
+password = pfile.read()[:-1] # pēdējais simbols ir rindas beigas pazīme, mums to nevajag
+pfile.close()
+@app.route("/temp")
+def temp():
+    db = MySQLdb.connect("localhost", user, password, "world")
+    cur = db.cursor()
+    query = "SELECT * FROM world.artists WHERE pkArtistId=1"
+    cur.execute(query)
+    return jsonify(data=cur.fetchall())
+if __name__ == '__main__':
+    app.run(port=5000)
